ae0bc952b297509d5ccfae73ec7351bf7ae96fea
wjkim96 <115263896+wjkim96@users.noreply.github.com>
2023-02-01 17:10:40 +0900

    Time: 77 ms (30.91%), Space: 18.9 MB (9.95%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -5,22 +5,17 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def height(self, root):
-        if not root:
-            return 0
-        left, right = self.height(root.left), self.height(root.right)
-        return max(left, right) + 1
-    def isBalancedHelp(self, root):
-        if not root:
-            return True
-        if abs(self.height(root.left) - self.height(root.right)) > 1:
-            return False
-        else:
-            return True
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def isBalancedHelp(root):
+            if not root:
+                return 0, True
+            left, right = isBalancedHelp(root.left), isBalancedHelp(root.right)
+            if abs(left[0] - right[0]) > 1:
+                return max(left[0], right[0]) + 1, False
+            return max(left[0], right[0]) + 1, True
         if not root:
             return True
-        if not self.isBalancedHelp(root):
+        if not isBalancedHelp(root)[1]:
             return False
         else:
             if not self.isBalanced(root.left):
@@ -28,6 +23,29 @@ class Solution:
             if not self.isBalanced(root.right):
                 return False
         return True
+    # def height(self, root):
+    #     if not root:
+    #         return 0
+    #     left, right = self.height(root.left), self.height(root.right)
+    #     return max(left, right) + 1
+    # def isBalancedHelp(self, root):
+    #     if not root:
+    #         return True
+    #     if abs(self.height(root.left) - self.height(root.right)) > 1:
+    #         return False
+    #     else:
+    #         return True
+    # def isBalanced(self, root: Optional[TreeNode]) -> bool:
+    #     if not root:
+    #         return True
+    #     if not self.isBalancedHelp(root):
+    #         return False
+    #     else:
+    #         if not self.isBalanced(root.left):
+    #             return False
+    #         if not self.isBalanced(root.right):
+    #             return False
+    #     return True
 
         
         
