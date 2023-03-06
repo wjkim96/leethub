e624b7690eea44527a1bdd9f3f4dff3653bcf24a
wjkim96 <115263896+wjkim96@users.noreply.github.com>
2023-03-06 12:54:58 +0900

    Time: 44 ms (32.92%), Space: 13.8 MB (76.51%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,9 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # for _ in range(len(nums1) -1 - m):
-            # nums1.pop()
-        nums1.insert(m, float("inf"))
+        for _ in range(len(nums1) - m):
+            nums1.pop()
+        nums1.append(float("inf"))
         print(nums1)
         while n > 0:
             x = nums2.pop(0)
@@ -14,7 +14,7 @@ class Solution:
             for i in range(len(nums1)):
                 if x <=nums1[i]:
                     nums1.insert(i, x)
-                    nums1.pop()
+                    # nums1.pop()
                     # print(nums1)
                     break;
             n -= 1
